f97599642352b60f301868e8075c4e479a02ab67
David Goehring <dggoeh1@mit.edu>
2013-01-24 23:34:41 -0500

    minor fixes

--- wrapper.py
+++ wrapper.py
@@ -216,7 +216,7 @@ class IRModule(threading.Thread):
             self.f.write(str(ir_val))
             self.f.write('\n')
             #print self.ir_val
-            time.sleep(0.1)
+            time.sleep(0.2)
 
     '''Get IR values. If filtered, gives a weighted average for noise reduction'''
     def getIRVal(self):
